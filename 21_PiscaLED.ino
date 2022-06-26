3200070a37df04387f94cacdd470abb02d3b1ae8
gapigo <x.gabriel12@gmail.com>
2022-06-26 17:35:12 -0300

    21 Pisca LED

---
+++ 21_PiscaLED.ino
@@ -0,0 +1,12 @@
+//Circuito: https://www.tinkercad.com/things/5EZRVoov2gt-brilliant-leelo-amberis
+
+void setup() {
+  pinMode(13, OUTPUT);
+}
+
+void loop(){
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(1000);
+}
